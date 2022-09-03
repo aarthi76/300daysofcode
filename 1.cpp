6e580653bdeee01d24f11c080ded8cb908a194b1
Aarthi <52964048+aarthi76@users.noreply.github.com>
2022-09-03 06:49:29 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,22 @@
+/* The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k. 
+
+*/
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& A, int K) {
+        for (int i = A.size() - 1; i >= 0 && K > 0; --i) {
+            A[i] += K;
+            K = A[i] / 10;
+            A[i] %= 10;
+        }
+        while (K > 0) {
+            A.insert(A.begin(), K % 10);
+            K /= 10;
+        }
+        return A;
+    }
+};
